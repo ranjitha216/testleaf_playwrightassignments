9d9294902549cabd783f2afa3484f856c3598795
ranjitha216 <73516493+ranjitha216@users.noreply.github.com>
2024-11-29 14:01:27 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+// Create the following variables using let (not using var) and check their typeOf 
+// a) firstName 
+// b) companyName 
+// c) mobileNumber 
+// d) isAutomation 
+// e) hasPlaywright (do not assign) 
+//Print and confirm the values and data types
+
+let firstName ='Ranjitha';
+console.log("The firstName is " + firstName);
+console.log(typeof(firstName));
+
+let companyName ='TechMahindra';
+console.log("The companyName is " + companyName);
+console.log(typeof(companyName));
+
+let mobileNumber= 99009999009;
+console.log("The mobileNumber is " +mobileNumber);
+console.log(typeof(mobileNumber));
+
+let isAutomation = true;
+console.log("the value of isAutomation is " +isAutomation);
+console.log(typeof(isAutomation));
+
+let hasPlaywright;
+console.log("the value of hasPlaywright is " +hasPlaywright);
+console.log(typeof(hasPlaywright));
